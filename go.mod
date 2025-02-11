1f5a37f6825fb85dbf9a8ca822310b2909441215
ProjectOrangeJuice <git@harriso.co.uk>
2025-02-11 21:04:53 +0000

    simple webhook listener

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ProjectOrangeJuice/memo-publisher
+
+go 1.22.2
---
+++ main.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+)
+
+func main() {
+
+	http.HandleFunc("/webhook", webhookHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+
+}
+
+type webhookData struct {
+	Activity string `json:"activityType"`
+	Memo     struct {
+		UID       string `json:"uid"`
+		Content   string `json:"content"`
+		Resources []struct {
+			Name     string `json:"name"`
+			Filename string `json:"filename"`
+		}
+		Visibility string `json:"visibility"`
+	} `json:"memo"`
+}
+
+func webhookHandler(w http.ResponseWriter, req *http.Request) {
+	log.Printf("Web hook")
+
+	// Read the body data
+	body, err := io.ReadAll(req.Body)
+	if err != nil {
+		log.Printf("Failed to read body, error: %v", err)
+		return
+	}
+
+	// Read the json data
+	var data webhookData
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		log.Printf("Failed to read json body, error: %v body(%s)", err, body)
+		return
+	}
+
+	log.Printf("Body data: %v", body)
+	log.Printf("Json data: %+v", data)
+
+	if data.Memo.Visibility == "PUBLIC" {
+		log.Printf("Not public, skipping")
+		return
+	}
+
+	if data.Activity == "memos.memo.deleted" {
+		// Delete the file
+		err = os.Remove(fmt.Sprintf("%s.md", data.Memo.UID))
+		if err != nil {
+			log.Printf("Failed to delete file, error: %v", err)
+			return
+		}
+		return
+	}
+
+	// Now we generate the template
+
+	// Find the first # element
+	heading, text := getFirstHashLineAndRemove(data.Memo.Content)
+	log.Printf("Heading: %s", heading)
+
+	// Generate the template
+	template := fmt.Sprintf(`
++++
+date = '%s'
+draft = false
+title = "%s"
++++
+	%s`, time.Now().Format("2006-01-02"), heading, text)
+
+	log.Printf("Template: %s", template)
+
+	// Write the template to a file
+	err = os.WriteFile(fmt.Sprintf("%s.md", data.Memo.UID), []byte(template), 0644)
+	if err != nil {
+		log.Printf("Failed to write file, error: %v", err)
+		return
+	}
+}
+
+func getFirstHashLineAndRemove(text string) (string, string) {
+	var firstHashLine string
+	var remainingText string
+	scanner := bufio.NewScanner(strings.NewReader(text))
+	var lines []string
+	found := false
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		if !found && strings.HasPrefix(line, "#") {
+			// Remove the #
+			line = strings.TrimPrefix(line, "#")
+			firstHashLine = line
+			found = true
+		} else {
+			lines = append(lines, line)
+		}
+	}
+	remainingText = strings.Join(lines, "\n")
+	return firstHashLine, remainingText
+}
